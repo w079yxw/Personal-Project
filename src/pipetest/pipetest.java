96b48b59c878943b3225423a53471e0d6bdb368d
yipengwang <yipengwang@yipengs-air>
2016-12-03 23:03:42 -0500

    Merge origin/master
    Conflicts:
    	index.php

--- src/pipetest/pipetest.java
+++ src/pipetest/pipetest.java
@@ -15,6 +15,8 @@ import java.io.*;
 import java.io.IOException;  
 import java.io.PipedInputStream;  
 import java.io.PipedOutputStream;  
+import java.util.logging.Level;
+import java.util.logging.Logger;
   
 /** 
  * 管道通信demo 
@@ -26,82 +28,90 @@ public class pipetest {
   
     public static void main(String[] args) throws IOException {  
   
-        // 创建管道输出流  
+       
         PipedOutputStream pos = new PipedOutputStream();  
-        // 创建管道输入流  
         PipedInputStream pis = new PipedInputStream();  
-        try {  
-            // 将管道输入流与输出流连接 此过程也可通过重载的构造函数来实现  
+        try {   
             pos.connect(pis);  
+            
         } catch (IOException e) {  
             e.printStackTrace();  
         }  
-        // 创建生产者线程  
-        Producer p = new pipetest().new Producer(pos);  
-        // 创建消费者线程  
+      Producer p = new pipetest().new Producer(pos);  
+        
         Consumer c = new Consumer(pis);  
-        // 启动线程  
         p.start();  
-        c.start();  
+        c.start();   
+       
     }  
   
-    // 生产者线程(与一个管道输入流相关联)  
+    
     private class Producer extends Thread   {  
         private PipedOutputStream pos;  
          Random random = new Random();
-        File file = new File("d.txt");
+        File file = new File("producer.txt");
           FileWriter writer; 
-          
 
 
         public Producer(PipedOutputStream pos)throws IOException  { 
              writer = new FileWriter(file);
                
-             
             this.pos = pos;  
            
         }  
         
         public void run() {  
-             
-            // while (true) {//加入此句将出现“java - IOException: Read end dead”异常  
+             int i=12;
            try{
-                 for (int idx = 1; idx <= 10; ++idx){
+                 for (int idx = 1; idx <= 100; ++idx){
                 int num=random.nextInt(100);
-                //System.out.println(num);
                 pos.write(num);
+                
                 writer.write(String.valueOf(num)+" ");
                  writer.flush();
                   
 
     }
-                  
+                  pos.close();
                  writer.close();
             } catch (IOException e) {  
                 e.printStackTrace();  
             }  
-            // }  
+           
   
         }  
     }  
   
-    // 消费者线程(与一个管道输入流相关联)  
     private static class Consumer extends Thread {  
         private PipedInputStream pis;  
-  
-        public Consumer(PipedInputStream pis) {  
+         File file = new File("cunsumer.txt");
+          FileWriter writer;
+        public Consumer(PipedInputStream pis) throws IOException {
+            super();  
+            writer = new FileWriter(file);
+            
             this.pis = pis;  
         }  
   
         public void run() {  
-            // while(true){//加入此句将出现“java - IOException: Write end dead”异常  
             try {  
-                System.out.println(pis.read()); 
-            } catch (IOException e) {  
+                
+                int num;
+                while ((num = pis.read()) >= 0) {
+                    System.out.println("consume:"+num);
+		
+                
+                writer.write(String.valueOf(num)+" ");
+                 writer.flush();	
+			}
+               pis.close(); 
+            }
+           
+            catch (IOException e) {  
                 e.printStackTrace();  
             }  
-            // }  
-  
+              
+                
         }  
     }  
 }  
